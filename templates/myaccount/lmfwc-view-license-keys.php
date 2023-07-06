41b87e461779172319a678261a4c620085ffe3b7
alejo2246 <alejopadilla297@gmail.com>
2023-07-06 01:10:35 -0500

    .

--- templates/myaccount/lmfwc-view-license-keys.php
+++ templates/myaccount/lmfwc-view-license-keys.php
@@ -6,6 +6,49 @@ defined('ABSPATH') || exit;
 ?>
 
 <h2><?php _e('Your license keys', 'license-manager-for-woocommerce'); ?></h2>
+<style>.modal {
+    display: none;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+}
+
+.modal-dialog {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+    border-radius: 5px;
+    padding: 20px;
+    width: 400px;
+    max-width: 90%;
+}
+
+.modal-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.modal-title {
+    margin: 0;
+}
+
+.close {
+    border: none;
+    cursor: pointer;
+}
+.container {
+                background-color: #f1f1f1; /* Color de fondo de la caja */
+                padding: 20px; /* Espacio interno alrededor del contenido */
+                margin-top: 20px; /* Margen superior de 10 píxeles */
+                border-top: 1px solid gray; /* Borde superior de 1 píxel de grosor y color negro */
+            }
+</style>
 
 <?php foreach ($licenseKeys as $productId => $licenseKeyData): ?>
     <?php $product = wc_get_product($productId); ?>
@@ -64,7 +107,7 @@ defined('ABSPATH') || exit;
                         <?php endif; ?>
 
                         <form action="..\giftcardform.php" method="post">
-                            <button class="button" type="submit" name="send"> Send</button>
+                            <button class="button" type="submit" name="send" id="btnOpenMod" data-license="<?php echo $license->getDecryptedLicenseKey();?>"> <?php _e('Send', 'license-manager-for-woocommerce');?></button>
                         </form>
                         <a href="<?php echo esc_url($order->get_view_order_url()); ?>" class="button view"><?php _e('Order', 'license-manager-for-woocommerce');?></a>
                     </div>
@@ -75,3 +118,147 @@ defined('ABSPATH') || exit;
         </tbody>
     </table>
 <?php endforeach; ?>
+
+
+
+<!-- Modal -->
+<button type="button" class="btn btn-primary" id="btnOpenMod">
+    Abrir Modal
+</button>
+
+<div class="modal" id="myModal" tabindex="-1" role="dialog">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Modal centrado en la mitad</h5>
+                <button type="button" class="close" id="btnCloseMod" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>Contenido del modal aquí.</p>
+            </div>
+                    <div class="container">
+            <h3>Send certificates</h3>
+            <label>Please, complete the following information to start giving the gift card</label> 
+                <div class="mb-3, mt-3">
+                    <label for="InputEmail" class="form-label">Email address of the recipient</label>
+                    <input type="email" name="email" class="form-control" id="InputEmail" aria-describedby="emailHelp">
+                    <div id="emailHelp" class="form-text">We'll send the gift card to this email address.</div>
+                </div>
+                <div class="mb-3">
+                    <label for="InputFrom" class="form-label">From</label>
+                    <input type="name" name="from" class="form-control" id="InputFrom" aria-details="nameHelp">
+                    <div id="from" class="form-text"></div>
+                </div>
+                <div class="mb-3">
+                    <label for="InputTo" class="form-label">To</label>
+                    <input type="name" name="to" class="form-control" id="InputTo">
+                </div>
+                <div class="mb-3" id="MessageInputDiv" style="display: none;">
+                    <label for="InputMessage" class="form-label">Personalized Message</label>
+                    <input type="name" name="message" class="form-control" id="InputMessage">
+                </div>
+                <div class="mb-3">
+                    <label for="IncludeMessage" class="form-check-label">Include a personalized message?</label>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" id="IncludeMessageCheckbox" onclick="toggleMessageInput()">
+                        <label class="form-check-label" for="IncludeMessageCheckbox">
+                            Yes
+                        </label>
+                    </div>
+                </div>
+
+                <div class="mb-3">
+                    <label for="InputLanguage" class="form-label">Which language would you like to send this certificate?</label>
+                    <select name="language" class="form-control" id="InputLanguage" onchange="changeFormAction()">
+                        <option value="english">English</option>
+                        <option value="spanish">Spanish</option>
+                    </select>
+                </div>
+                <!-- <div class="mb-3">
+                    <<label for="InputImage" class="form-label">Select an image</label>
+<input type="file" class="form-control" id="InputImage">
+</div> -->
+<button id="enviarCorreo" name="send" class="btn btn-primary">Submit</button>
+
+
+
+</div>
+        </div>
+    </div>
+</div>
+
+<script>
+    function toggleMessageInput() {
+        var messageInputDiv = document.getElementById("MessageInputDiv");
+        var includeMessageCheckbox = document.getElementById("IncludeMessageCheckbox");
+        
+        if (includeMessageCheckbox.checked) {
+            messageInputDiv.style.display = "block";
+        } else {
+            messageInputDiv.style.display = "none";
+        }
+    }
+    function changeFormAction() {
+        var languageSelect = document.getElementById("InputLanguage");
+        var form = document.getElementById("certificateForm");
+        let url = "";
+        if (languageSelect.value === "english") {
+            url = "../../wp-content/plugins/license-manager-for-woocommerce/email/enviar_correo_english.php";
+        } else if (languageSelect.value === "spanish") {
+            url = "../../wp-content/plugins/license-manager-for-woocommerce/email/enviar_correo_spanish.php";
+        }
+        return url;
+    }
+    function enviarCorreoElectronico(destinatario, from, to, message, url, urlToPost) {
+        const data = {
+            destinatario,
+            from,
+            to,
+            message,
+            url
+        };
+        fetch(urlToPost, {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+            'Content-Type': 'application/json'
+            }
+        })
+        .then(response => response.text())
+        .then(data => {
+            alert('correo enviado');
+        })
+        .catch(error => {
+            console.error(error); // Manejar error de la solicitud
+        });
+    }
+
+    var btnOpenMod = document.getElementById('btnOpenMod');
+    var btnCloseMod = document.getElementById('btnCloseMod');
+    var modal = document.getElementById('myModal');
+
+    btnOpenMod.addEventListener('click', function() {
+        modal.style.display = 'block';
+    });
+
+    btnCloseMod.addEventListener('click', function() {
+        modal.style.display = 'none';
+    });
+
+    var sendMail = document.getElementById('enviarCorreo');
+
+    sendMail.addEventListener('click', function(e) {
+        e.preventDefault();
+        console.log(sendMail.getAttribute('data-license'));
+        let destinatario = document.getElementById('InputEmail').value;
+        let from = document.getElementById('InputFrom').value;
+        let to = document.getElementById('InputTo').value;
+        let url = "https://as2.ftcdn.net/v2/jpg/02/67/11/55/1000_F_267115523_nhJWtLVlhtYtqGkfVOIzhOCAjQRrejVI.jpg";
+        let message = document.getElementById('InputMessage').value;
+        let urlToPost = changeFormAction();
+        enviarCorreoElectronico(destinatario, from, to, message, url, urlToPost);
+    });
+
+</script>
